eb065166b0dca90526f4d97d076c326e8603dce5
elias-barroso <32077258+elias-barroso@users.noreply.github.com>
2017-12-06 11:38:59 -0300

    Adicionado node-link Lesmiserables

---
+++ VisNetPartidos.js
@@ -0,0 +1,12 @@
+
+      // Create network;
+      // Create network;
+      var network = Network();
+      
+      //Read json data and send data to network
+      d3.json("data/lesmiserables.json", function(error, graph) {
+        if (error) throw error;
+        return network("#vis", graph);
+      });
+
+
